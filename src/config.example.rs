35028aee15cf876cf5c95d74b4e2a47a489eb75e
Benjamin Pannell <benjamin@pannell.dev>
2024-10-09 10:47:55 +0100

    fix: Remove unused timezone config

--- src/config.example.rs
+++ src/config.example.rs
@@ -6,4 +6,3 @@ pub const TODOIST_FILTER: &str = "today & !subtask & (!shared | assigned to:me)"
 
 // https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv for a list of valid timezones
 pub const TIMEZONE_SPEC: &str = "UTC0";
-pub const TIMEZONE_OFFSET: i32 = 0;
